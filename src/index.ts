7d1b9f1cc1f4c95299cebd605b5040d186613c85
Nukie <92524294+NukieGIT@users.noreply.github.com>
2022-10-03 00:15:22 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+class Main {
+    public static main(): void {}
+}
 
-import confetti from 'canvas-confetti';
+Main.main();
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+export {};
